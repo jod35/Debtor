f4cba026f9879f011dac6587408a7a39ecad1618
jon <jon@localhost.localdomain>
2019-12-24 01:46:31 +0300

    sign up form done

--- debtor/forms.py
+++ debtor/forms.py
@@ -1,4 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired,Email,Length,EqualTo
-from wtforms import StringField,PasswordField,TextAreaField
+from wtforms import StringField,PasswordField,TextAreaField,SubmitField
+
+class RegisterForm(FlaskForm):
+    name=StringField("Full Name",validators=[DataRequired(),Length(min=4,max=40)])
+    username=StringField("Username",validators=[DataRequired(),Length(max=10)])
+    email=StringField("Email",validators=[Email("Invalid Email"),Length(max=80)])
+    password=StringField("Password",validators=[DataRequired(),EqualTo('confirm',message="Passwords Do Not Match")])
+    password=StringField("Password")
+    submit=SubmitField("Sign Up")
 
