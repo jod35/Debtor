3fd34ddd94333c76bee4509080a06d7be2066425
jon <you@example.com>
2019-12-25 02:12:44 +0300

    account creation works perfect

--- debtor/views.py
+++ debtor/views.py
@@ -1,7 +1,8 @@
-from flask import render_template,redirect,url_for,request
+from flask import render_template,redirect,url_for,request,flash
 from werkzeug.security import generate_password_hash,check_password_hash
 from . import login_manager,db,app
 from .forms import LoginForm,RegisterForm
+from .models import User
 
 
 @app.route('/debtors')
@@ -9,9 +10,30 @@ def index():
     form=LoginForm()
     return render_template('index.html',form=form)
 
-@app.route('/debtors/signup')
+@app.route('/debtors/signup',methods=['GET', 'POST'])
 def create_account():
+
     form=RegisterForm()
+
+    if form.validate_on_submit():
+        name=request.form.get('name')
+        username=request.form.get('username')
+        email=request.form.get('email')
+        pass_hash=request.form.get('password')
+
+        args={
+            'name':name,
+            'username':username,
+            'email':email,
+            'pass_hash':generate_password_hash(pass_hash)
+        }
+
+        new_user=User(**args)
+        db.session.add(new_user)
+        db.session.commit()
+        flash("User Account Created Successfully!!")
+        return redirect(url_for('create_account'))
+
     return render_template('sign.html',form=form)
 
 @app.route('/debtors/list')
