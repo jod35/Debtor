b4d7b946322951c061dde6b876fd6745df141be2
jon <you@example.com>
2020-01-14 15:44:15 +0300

    protected some views with login required decorator

--- debtor/__init__.py
+++ debtor/__init__.py
@@ -2,7 +2,7 @@ from .config import Config
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_migrate import Migrate
+#from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,7 +13,7 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-migrate = Migrate(app, db)
+#migrate = Migrate(app, db)
 
 
 from . import views
--- debtor/views.py
+++ debtor/views.py
@@ -1,11 +1,12 @@
 from flask import render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import login_manager, db, app
-from .forms import LoginForm, RegisterForm,DebtorCreationForm
-from .models import User,Debt
+from .forms import LoginForm, RegisterForm, DebtorCreationForm
+from .models import User, Debt
 from flask_login import login_user, logout_user, current_user, login_required
 from datetime import date
 
+login_manager.login_view = 'index'
 @app.route('/debtors', methods=['GET', 'POST'])
 def index():
     form = LoginForm()
@@ -22,6 +23,7 @@ def index():
 
 
 @app.route('/debtors/signup', methods=['GET', 'POST'])
+@login_required
 def create_account():
 
     form = RegisterForm()
@@ -49,96 +51,116 @@ def create_account():
 
 
 @app.route('/debtors/dashboard')
+@login_required
 def user_dashboard():
     return render_template('lists.html')
+
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
 
-@app.route('/debtors/add',methods=['GET', 'POST'])
+
+@app.route('/debtors/add', methods=['GET', 'POST'])
+@login_required
 def add_debts():
-    form=DebtorCreationForm()
-    today=date.today()
+    form = DebtorCreationForm()
+    today = date.today()
 
     if request.method == 'POST':
-        args={
-            'supplier_name':request.form.get('s_name'),
-            'date_supplied':request.form.get('date_supplied'),
-            'amount_paid':request.form.get('amount_paid'),
-            'debt_amount':request.form.get('debt_amount'),
-            'cleared':False,
-            'balance':(int(request.form.get('debt_amount'))-int(request.form.get('amount_paid')))
+        args = {
+            'supplier_name': request.form.get('s_name'),
+            'date_supplied': request.form.get('date_supplied'),
+            'amount_paid': request.form.get('amount_paid'),
+            'debt_amount': request.form.get('debt_amount'),
+            'cleared': False,
+            'balance': (int(request.form.get('debt_amount')) - int(request.form.get('amount_paid')))
         }
 
-        new_debt=Debt(**args)
+        new_debt = Debt(**args)
         db.session.add(new_debt)
         db.session.commit()
         flash("Record Has Been Saved Successfully")
         return redirect(url_for('add_debts'))
-    return render_template('adddebt.html',form=form,today=today)
+    return render_template('adddebt.html', form=form, today=today)
+
 
 @app.route('/debtors/lists')
+@login_required
 def view_debts():
-    
-    context={
-        'debts':Debt.query.all()
-    }
-    return render_template('debts.html',**context)
 
-#render the update template
+    context = {
+        'debts': Debt.query.all()
+    }
+    return render_template('debts.html', **context)
+
+# render the update template
+
+
 @app.route('/debtors/details/debt_<int:debt_id>')
+@login_required
 def debt_details(debt_id):
-    
 
-    context={
-        'debt_to_update':Debt.query.get_or_404(debt_id)
+    context = {
+        'debt_to_update': Debt.query.get_or_404(debt_id)
     }
-    return render_template('details.html',**context)
+    return render_template('details.html', **context)
 
-#update a record basing on its id
-@app.route('/update/<int:id>',methods=['POST'])
+# update a record basing on its id
+
+
+@app.route('/update/<int:id>', methods=['POST'])
+@login_required
 def update_record(id):
-    record_to_update=Debt.query.get_or_404(id)
+    record_to_update = Debt.query.get_or_404(id)
     record_to_update.supplier_name = request.form.get('s_name')
-    record_to_update.date_supplied=request.form.get('date_supplied')
-    record_to_update.debt_amount=request.form.get('debt_amount')
-    record_to_update.amount_paid=request.form.get('amount_paid')
-    record_to_update.balance=(int(request.form.get('debt_amount'))-int(request.form.get('amount_paid')))
+    record_to_update.date_supplied = request.form.get('date_supplied')
+    record_to_update.debt_amount = request.form.get('debt_amount')
+    record_to_update.amount_paid = request.form.get('amount_paid')
+    record_to_update.balance = (int(request.form.get(
+        'debt_amount')) - int(request.form.get('amount_paid')))
 
     db.session.commit()
     return redirect(url_for('view_debts'))
 
-#delete a record basing on the ID
-@app.route('/delete/<int:id>',methods=['GET', 'POST'])
+# delete a record basing on the ID
+
+
+@app.route('/delete/<int:id>', methods=['GET', 'POST'])
+@login_required
 def delete(id):
-    debt_to_delete=Debt.query.get_or_404(id)
+    debt_to_delete = Debt.query.get_or_404(id)
     db.session.delete(debt_to_delete)
     db.session.commit()
     return redirect(url_for('view_debts'))
 
-    
-@app.route('/cleared/<int:id>',methods=['POST'])
+
+@app.route('/cleared/<int:id>', methods=['POST'])
+@login_required
 def set_to_cleared(id):
-    debt_cleared=Debt.query.get_or_404(id)
-    debt_cleared.cleared =True
+    debt_cleared = Debt.query.get_or_404(id)
+    debt_cleared.cleared = True
 
     db.session.commit()
     return redirect(url_for('view_debts'))
 
+
 @app.route('/debtor/cleared')
+@login_required
 def cleared_invoices():
-    debts=Debt.query.filter_by(cleared=True).all()
-    context={
-        'debts':debts
+    debts = Debt.query.filter_by(cleared=True).all()
+    context = {
+        'debts': debts
     }
-    return render_template('cleared.html',**context)
+    return render_template('cleared.html', **context)
+
 
 @app.route('/debtor/unpaid')
+@login_required
 def unpaid_invoices():
-    debts=Debt.query.filter_by(cleared=False).all()
-    context={
-        'debts':debts
+    debts = Debt.query.filter_by(cleared=False).all()
+    context = {
+        'debts': debts
     }
-    return render_template('uncleared.html',**context)
+    return render_template('uncleared.html', **context)
